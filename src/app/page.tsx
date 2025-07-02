4c4cef1bfd6dddb22a887ceb5ba838acefe17115
Rao Pushpender Yaduvanshi <rpyaduvanshi950@gmail.com>
2025-07-02 13:19:35 +0000

    at bottom write developed by sign speak team

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
       <footer className="py-6 md:px-8 md:py-0 border-t">
         <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Powered by Next.js.
+            developed by sign speak team
           </p>
         </div>
       </footer>
