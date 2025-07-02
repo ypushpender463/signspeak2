3e5e396e3ed15be426aa11ed5591b904ffe06e81
Rao Pushpender Yaduvanshi <rpyaduvanshi950@gmail.com>
2025-07-02 13:20:29 +0000

    and make the data in center not in the on side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
       </main>
       <footer className="py-6 md:px-8 md:py-0 border-t">
         <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+          <p className="text-center text-sm leading-loose text-muted-foreground">
             developed by sign speak team
           </p>
         </div>
